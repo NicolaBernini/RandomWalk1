4a25cf800f3bbfe9b1d9a413ff3d976a376592cd
Nicola Bernini <NicolaBernini@users.noreply.github.com>
2016-01-13 01:49:11 +0100

    Create RandomVariables.h

---
+++ RW1/src/RandomVariables.h
@@ -0,0 +1,18 @@
+
+#ifndef RANDOMVARIABLES_H
+#define RANDOMVARIABLES_H
+
+namespace RandomVarNS
+{
+  /**
+    * @brief The generic \f$ X \f$ Random Variable with values over a certain domain
+    */
+    template <typename Domain>
+    class RandomVariable
+    {
+
+    };
+}
+
+
+#endif
